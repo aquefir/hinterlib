2fc2132b06d099d5cc3bf8cdf57ca0219252bbb9
Alexander Nicholi <me@nicholatian.com>
2021-03-04 20:43:58 -0500

    bring over helper scripts from neopolitan

---
+++ common/util/cleanall.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# -*- coding: utf-8 -*-
+
+if test "$(uname -s)" = 'Darwin'; then
+	_make=gmake;
+	if command -v gecho >/dev/null 2>&1; then
+		_echo=gecho;
+	else
+		_echo='/bin/echo'; # ensure it is not a bashism
+	fi
+else
+	_make=make;
+	_echo='/bin/echo'; # ensure it is not a bashism
+fi
+
+${_echo} -e 'Cleaning all sub-repositories.\n' >/dev/stderr;
+
+_repos='arr clarg err futils himem log mt19937 str table';
+
+for _repo in ${_repos}; do
+	cd ${_repo};
+	${_echo} -n "Cleaning sub-repo ‘${_repo}’... " >/dev/stderr;
+	${_make} clean 2>&1 >/dev/null;
+	${_echo} 'done.' >/dev/stderr;
+	cd ..;
+done
+
+${_echo} 'All done. Exiting...' >/dev/stderr;
+unset _repo _repos _echo _make;
---
+++ common/util/install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# -*- coding: utf-8 -*-
+
+if test "$(uname -s)" = 'Darwin'; then
+	_make=gmake;
+	if command -v gecho >/dev/null 2>&1; then
+		_echo=gecho;
+	else
+		_echo='/bin/echo'; # ensure it is not a bashism
+	fi
+else
+	_make=make;
+	_echo='/bin/echo'; # ensure it is not a bashism
+fi
+
+if test "$(id -u)" = '0'; then
+	_sudo='';
+else
+	_sudo='sudo ';
+fi
+
+common/util/cleanall.sh;
+common/util/makeall.sh release;
+
+if test "${PREFIX}" = ''; then
+	PREFIX='/usr/local/x86_64-pc-ape';
+fi
+
+_prefix="${PREFIX}";
+
+${_echo} -e 'Installing all sub-repositories.\n' >/dev/stderr;
+
+_repos='arr chkmath clarg decl endian err futils himem log mt19937 str table';
+
+for repo in ${_repos}; do
+	cd ${repo};
+	${_echo} "Installing sub-repo ‘${repo}’..." >/dev/stderr;
+	${_sudo} ${_make} install PREFIX="${_prefix}" AQ=/opt/aq;
+	cd ..;
+done
+
+${_echo} 'All done. Exiting...' >/dev/stderr;
+
+unset _repo _repos _sudo _echo _make;
+exit 0;
