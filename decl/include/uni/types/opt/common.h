02b88716d5f4ec8aac68f119f7274fb659e65f3b
Alexander Nicholi <me@nicholatian.com>
2021-03-24 00:49:49 -0400

    modify declarator macros to make token pasting work when given macros as input names

--- decl/include/uni/types/opt/common.h
+++ decl/include/uni/types/opt/common.h
@@ -11,28 +11,28 @@
 #define UNI_OPTION( TYPE ) struct opt_##TYPE
 #define UNI_OPTIONP( TYPE ) struct optp_##TYPE
 #define UNI_DECL_OPT( TYPE ) \
-	UNI_OPTION( TYPE ) \
+	struct opt_##TYPE \
 	{ \
 		TYPE val; \
 		unsigned char is; \
 	}; \
 	struct _dummy
 #define UNI_DECL_OPT_S( TYPE ) \
-	UNI_OPTION( TYPE ) \
+	struct opt_##TYPE \
 	{ \
 		struct TYPE val; \
 		unsigned char is; \
 	}; \
 	struct _dummy
 #define UNI_DECL_OPT_P( TYPE ) \
-	UNI_OPTIONP( TYPE ) \
+	struct optp_##TYPE \
 	{ \
 		TYPE * val; \
 		unsigned char is; \
 	}; \
 	struct _dummy
 #define UNI_DECL_OPT_SP( TYPE ) \
-	UNI_OPTIONP( TYPE ) \
+	struct optp_##TYPE \
 	{ \
 		struct TYPE * val; \
 		unsigned char is; \
