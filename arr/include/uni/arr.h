475cec359b60ba4fca530e00fcf93f4e2ce64de3
Alexander Nicholi <me@nicholatian.com>
2022-05-09 18:56:38 -0400

    most data structs are in now

--- arr/include/uni/arr.h
+++ arr/include/uni/arr.h
@@ -15,50 +15,283 @@
 
 UNI_DEPRECATED struct uni_arr;
 
-struct uni_slist_node;
+struct uni_1ll_node;
 
 /* node of a singly linked list. */
-struct uni_slist_node
+struct uni_1ll_node
 {
 	/* pointer to the node's data. */
 	void * data;
 	/* size of the node's data, denominated in octets. */
 	ptri sz;
 	/* the next node in this singly linked list. */
-	struct uni_slist_node * next;
+	struct uni_1ll_node * next;
 };
 
-/* singly linked list. */
-struct uni_slist
+struct uni_c1ll_node;
+
+/* node of a circular singly linked list. */
+struct uni_c1ll_node
 {
-	/* head (first element) of the singly linked list. */
-	struct uni_slist_node * head;
-	/* tail (last element) of the singly linked list. */
-	struct uni_slist_node * tail;
+	/* pointer to the node's data. */
+	void * data;
+	/* size of the node's data, denominated in octets. */
+	ptri sz;
+	/* the next node in this circular singly linked list. */
+	struct uni_c1ll_node * next;
 };
 
-struct uni_dlist_node;
+struct uni_sc1ll_node;
+
+/* node of a sentinel-bearing circular singly linked list. */
+struct uni_sc1ll_node
+{
+	/* pointer to the node's data. */
+	void * data;
+	/* size of the node's data, denominated in octets. */
+	ptri sz;
+	/* the next node in this sentinel-bearing circular singly linked list.
+	 */
+	struct uni_sc1ll_node * next;
+};
+
+struct uni_2ll_node;
 
 /* node of a doubly linked list. */
-struct uni_dlist_node
+struct uni_2ll_node
 {
 	/* pointer to the node's data. */
 	void * data;
 	/* size of the node's data, denominated in octets. */
 	ptri sz;
 	/* the previous node in this doubly linked list. */
-	struct uni_dlist_node * prev;
+	struct uni_2ll_node * prev;
 	/* the next node in this doubly linked list. */
-	struct uni_dlist_node * next;
+	struct uni_2ll_node * next;
 };
 
-/* doubly linked list. */
-struct uni_dlist
+struct uni_c2ll_node;
+
+/* node of a circular doubly linked list. */
+struct uni_c2ll_node
 {
-	/* head (first element) of this doubly linked list. */
-	struct uni_dlist_node * head;
-	/* tail (last element) of this singly linked list. */
-	struct uni_dlist_node * tail;
+	/* pointer to the node's data. */
+	void * data;
+	/* size of the node's data, denominated in octets. */
+	ptri sz;
+	/* the previous node in this circular doubly linked list. */
+	struct uni_c2ll_node * prev;
+	/* the next node in this circular doubly linked list. */
+	struct uni_c2ll_node * next;
+};
+
+struct uni_sc2ll_node;
+
+/* node of a sentinel-bearing circular doubly linked list. */
+struct uni_sc2ll_node
+{
+	/* pointer to the node's data. */
+	void * data;
+	/* size of the node's data, denominated in octets. */
+	ptri sz;
+	/* the previous node in this sentinel-bearing circular doubly linked
+	 * list. */
+	struct uni_sc2ll_node * prev;
+	/* the next node in this sentinel-bearing circular doubly linked list.
+	 */
+	struct uni_sc2ll_node * next;
+};
+
+struct uni_3ll_node;
+
+/* node of a triply linked list. */
+struct uni_3ll_node
+{
+	/* pointer to the node's data. */
+	void * data;
+	/* size of the node's data, denominated in octets. */
+	ptri sz;
+	/* the primary neighbour node in this triply linked list. */
+	struct uni_3ll_node * prima;
+	/* the secondary neighbour node in this triply linked list. */
+	struct uni_3ll_node * secunda;
+	/* the tertiary neighbour node in this triply linked list. */
+	struct uni_3ll_node * tertia;
+};
+
+struct uni_c3ll_node;
+
+/* node of a circular triply linked list. */
+struct uni_c3ll_node
+{
+	/* pointer to the node's data. */
+	void * data;
+	/* size of the node's data, denominated in octets. */
+	ptri sz;
+	/* the primary neighbour node in this circular triply linked list. */
+	struct uni_c3ll_node * prima;
+	/* the secondary neighbour node in this circular triply linked list. */
+	struct uni_c3ll_node * secunda;
+	/* the tertiary neighbour node in this circular triply linked list. */
+	struct uni_c3ll_node * tertia;
+};
+
+struct uni_sc3ll_node;
+
+/* node of a sentinel-bearing circular triply linked list. */
+struct uni_sc3ll_node
+{
+	/* pointer to the node's data. */
+	void * data;
+	/* size of the node's data, denominated in octets. */
+	ptri sz;
+	/* the primary neighbour node in this sentinel-bearing circular triply
+	 * linked list. */
+	struct uni_sc3ll_node * prima;
+	/* the secondary neighbour node in this sentinel-bearing circular
+	 * triply linked list. */
+	struct uni_sc3ll_node * secunda;
+	/* the tertiary neighbour node in this sentinel-bearing circular triply
+	 * linked list. */
+	struct uni_sc3ll_node * tertia;
+};
+
+struct uni_4ll_node;
+
+/* node of a quadruply linked list. */
+struct uni_4ll_node
+{
+	/* pointer to the node's data. */
+	void * data;
+	/* size of the node's data, denominated in octets. */
+	ptri sz;
+	/* the primary neighbour node in this quadruply linked list. */
+	struct uni_4ll_node * prima;
+	/* the secondary neighbour node in this quadruply linked list. */
+	struct uni_4ll_node * secunda;
+	/* the tertiary neighbour node in this quadruply linked list. */
+	struct uni_4ll_node * tertia;
+	/* the quaternary neighbour node in this quadruply linked list. */
+	struct uni_4ll_node * quarta;
+};
+
+struct uni_c4ll_node;
+
+/* node of a circular quadruply linked list. */
+struct uni_c4ll_node
+{
+	/* pointer to the node's data. */
+	void * data;
+	/* size of the node's data, denominated in octets. */
+	ptri sz;
+	/* the primary neighbour node in this circular quadruply linked list.
+	 */
+	struct uni_c4ll_node * prima;
+	/* the secondary neighbour node in this circular quadruply linked list.
+	 */
+	struct uni_c4ll_node * secunda;
+	/* the tertiary neighbour node in this circular quadruply linked list.
+	 */
+	struct uni_c4ll_node * tertia;
+	/* the quaternary neighbour node in this circular quadruply linked
+	 * list. */
+	struct uni_4ll_node * quarta;
+};
+
+struct uni_sc4ll_node;
+
+/* node of a sentinel-bearing circular quadruply linked list. */
+struct uni_sc4ll_node
+{
+	/* pointer to the node's data. */
+	void * data;
+	/* size of the node's data, denominated in octets. */
+	ptri sz;
+	/* the primary neighbour node in this sentinel-bearing circular
+	 * quadruply linked list. */
+	struct uni_sc4ll_node * prima;
+	/* the secondary neighbour node in this sentinel-bearing circular
+	 * quadruply linked list. */
+	struct uni_sc4ll_node * secunda;
+	/* the tertiary neighbour node in this sentinel-bearing circular
+	 * quadruply linked list. */
+	struct uni_sc4ll_node * tertia;
+	/* the quaternary neighbour node in this sentinel-bearing circular
+	 * quadruply linked list. */
+	struct uni_sc4ll_node * quarta;
+};
+
+struct uni_5ll_node;
+
+/* node of a quintuply linked list. */
+struct uni_5ll_node
+{
+	/* pointer to the node's data. */
+	void * data;
+	/* size of the node's data, denominated in octets. */
+	ptri sz;
+	/* the primary neighbour node in this quintuply linked list. */
+	struct uni_5ll_node * prima;
+	/* the secondary neighbour node in this quintuply linked list. */
+	struct uni_5ll_node * secunda;
+	/* the tertiary neighbour node in this quintuply linked list. */
+	struct uni_5ll_node * tertia;
+	/* the quaternary neighbour node in this quintuply linked list. */
+	struct uni_5ll_node * quarta;
+	/* the quinary neighbour node in this quintuply linked list. */
+	struct uni_5ll_node * quinta;
+};
+
+struct uni_c5ll_node;
+
+/* node of a circular quintuply linked list. */
+struct uni_c5ll_node
+{
+	/* pointer to the node's data. */
+	void * data;
+	/* size of the node's data, denominated in octets. */
+	ptri sz;
+	/* the primary neighbour node in this circular quintuply linked list.
+	 */
+	struct uni_c5ll_node * prima;
+	/* the secondary neighbour node in this circular quintuply linked list.
+	 */
+	struct uni_c5ll_node * secunda;
+	/* the tertiary neighbour node in this circular quintuply linked list.
+	 */
+	struct uni_c5ll_node * tertia;
+	/* the quaternary neighbour node in this circular quintuply linked
+	 * list. */
+	struct uni_5ll_node * quarta;
+	/* the quinary neighbour node in this circular quintuply linked list.
+	 */
+	struct uni_5ll_node * quinta;
+};
+
+struct uni_sc5ll_node;
+
+/* node of a sentinel-bearing circular quintuply linked list. */
+struct uni_sc5ll_node
+{
+	/* pointer to the node's data. */
+	void * data;
+	/* size of the node's data, denominated in octets. */
+	ptri sz;
+	/* the primary neighbour node in this sentinel-bearing circular
+	 * quintuply linked list. */
+	struct uni_sc5ll_node * prima;
+	/* the secondary neighbour node in this sentinel-bearing circular
+	 * quintuply linked list. */
+	struct uni_sc5ll_node * secunda;
+	/* the tertiary neighbour node in this sentinel-bearing circular
+	 * quintuply linked list. */
+	struct uni_sc5ll_node * tertia;
+	/* the quaternary neighbour node in this sentinel-bearing circular
+	 * quintuply linked list. */
+	struct uni_sc5ll_node * quarta;
+	/* the quinary neighbour node in this sentinel-bearing circular
+	 * quintuply linked list. */
+	struct uni_sc5ll_node * quinta;
 };
 
 /* hash table. */
@@ -76,7 +309,7 @@ struct uni_htbl
 	ptri cap;
 };
 
-/* circular buffer, useful for LIFO-style queues. */
+/* circular buffer, useful for FIFO-style queues. */
 struct uni_circbuf
 {
 	/* start of the physical buffer in memory. */
@@ -91,7 +324,7 @@ struct uni_circbuf
 	ptri elemsz;
 };
 
-/* flat buffer, useful for  vectors and FIFO-style stacks. */
+/* flat buffer, useful for vectors and LIFO-style stacks. */
 struct uni_flatbuf
 {
 	/* raw buffer data. */
@@ -104,14 +337,118 @@ struct uni_flatbuf
 	ptri elemsz;
 };
 
+struct uni_rb_node;
+
+/* node of a red-black tree. */
+struct uni_rb_node
+{
+	/* left child of this red-black node. */
+	struct uni_rb_node * lchild;
+	/* right child of this red-black node. */
+	struct uni_rb_node * rchild;
+	/* pseudo-pointer to the data of this red-black node.
+	 * the lowest bit contains the colour of the node: 1 == RED.
+	 *
+	 * IMPORTANT: clear the lowest bit of this before casting it to a
+	 * pointer type! */
+	ptri data;
+};
+
+struct uni_prb_node;
+
+/* node of a parental red-black tree. */
+struct uni_prb_node
+{
+	/* left child of this parental red-black node. */
+	struct uni_prb_node * lchild;
+	/* right child of this parental red-black node. */
+	struct uni_prb_node * rchild;
+	/* parent of this parental red-black node. */
+	struct uni_prb_node * parent;
+	/* pseudo-pointer to the data of this parental red-black node.
+	 * the lowest bit contains the colour of the node: 1 == RED.
+	 *
+	 * IMPORTANT: clear the lowest bit of this before casting it to a
+	 * pointer type! */
+	ptri data;
+};
+
+struct uni_avl_node;
+
+/* node of an AVL tree. */
+struct uni_avl_node
+{
+	/* left child of this AVL node. */
+	struct uni_avl_node * lchild;
+	/* right child of this AVL node. */
+	struct uni_avl_node * rchild;
+	/* pseudo-pointer to the data of this AVL node.
+	 * the lowest 2 bits contain the balance factor, in one's complement,
+	 * i.e. 0b00 == 0, 0b01 == 1, 0b10 == -0, and 0b11 == -1.
+	 *
+	 * IMPORTANT: clear the lowest 2 bits of this before casting it to a
+	 * pointer type! */
+	ptri data;
+};
+
+struct uni_pavl_node;
+
+/* node of a parental AVL tree. */
+struct uni_pavl_node
+{
+	/* left child of this parental AVL node. */
+	struct uni_pavl_node * lchild;
+	/* right child of this parental AVL node. */
+	struct uni_pavl_node * rchild;
+	/* parent of this parental AVL node. */
+	struct uni_pavl_node * parent;
+	/* pseudo-pointer to the data of this parental AVL node.
+	 * the lowest 2 bits contain the balance factor, in one's complement,
+	 * i.e. 0b00 == 0, 0b01 == 1, 0b10 == -0, and 0b11 == -1.
+	 *
+	 * IMPORTANT: clear the lowest 2 bits of this before casting it to a
+	 * pointer type! */
+	ptri data;
+};
+
+struct uni_splay_node;
+
+/* node of a splay tree. */
+struct uni_splay_node
+{
+	/* left child of this splay node. */
+	struct uni_splay_node * lchild;
+	/* right child of this splay node. */
+	struct uni_splay_node * rchild;
+	/* pointer to the node's data. */
+	void * data;
+};
+
+struct uni_psplay_node;
+
+/* node of a parental splay tree. */
+struct uni_psplay_node
+{
+	/* left child of this parental splay node. */
+	struct uni_psplay_node * lchild;
+	/* right child of this parental splay node. */
+	struct uni_psplay_node * rchild;
+	/* parent of this parental splay node. */
+	struct uni_psplay_node * parent;
+	/* pointer to the node's data. */
+	void * data;
+};
+
 UNI_C_API UNI_DEPRECATED struct uni_arr * uni_arr_init( u32 );
 UNI_C_API UNI_DEPRECATED struct uni_arr * uni_arr_initsz( u32, u32 );
 UNI_C_API UNI_DEPRECATED struct uni_arr * uni_arr_dup( struct uni_arr * );
 UNI_C_API UNI_DEPRECATED void * uni_arr_make( struct uni_arr * );
 UNI_C_API UNI_DEPRECATED void * uni_arr_mkslice( struct uni_arr *, rptri );
-UNI_C_API UNI_DEPRECATED struct uni_arr * uni_arr_conc( struct uni_arr *, ... );
+UNI_C_API UNI_DEPRECATED struct uni_arr * uni_arr_conc(
+	struct uni_arr *, ... );
 UNI_C_API UNI_DEPRECATED struct uni_arr * uni_arr_concv( struct uni_arr ** );
-UNI_C_API UNI_DEPRECATED struct uni_arr * uni_arr_slice( struct uni_arr *, rptri );
+UNI_C_API UNI_DEPRECATED struct uni_arr * uni_arr_slice(
+	struct uni_arr *, rptri );
 UNI_C_API UNI_DEPRECATED void uni_arr_fini( struct uni_arr * );
 
 UNI_C_API UNI_DEPRECATED ptri uni_arr_getsz( struct uni_arr * );
