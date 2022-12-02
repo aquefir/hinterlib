853eeacd63b2c630569479d71af8ae2be102dc3b
Alexander Nicholi <me@nicholatian.com>
2022-12-02 16:45:33 -0500

    impl alloca() and stub out others

--- include/uni/memory.h
+++ include/uni/memory.h
@@ -12,10 +12,26 @@
 
 void * uni_alloc( ptri );
 void * uni_alloc0( ptri );
+void * uni_alloca( ptri );
+#if 0
+void * uni_allocb( ptri );
+#endif /* 0 */
 void * uni_realloc( void *, ptri );
+#if 0
+void * uni_realloc0( void *, ptri );
+void * uni_reallocb( void *, ptri );
+#endif /* 0 */
 void * uni_tryalloc( ptri );
 void * uni_tryalloc0( ptri );
+#if 0
+void * uni_tryalloca( ptri );
+void * uni_tryallocb( ptri );
+#endif /* 0 */
 void * uni_tryrealloc( void *, ptri );
+#if 0
+void * uni_tryrealloc0( void *, ptri );
+void * uni_tryreallocb( void *, ptri );
+#endif /* 0 */
 void uni_free( void * );
 
 void uni_memset( void *, u8, ptri );
