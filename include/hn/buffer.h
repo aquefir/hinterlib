428840ac7ee9a6c25c02d292daa31245760308f8
Alexander Nicholi <me@nicholatian.com>
2023-01-04 16:24:24 -0500

    first wip on buffer API

---
+++ include/hn/buffer.h
@@ -0,0 +1,20 @@
+/*****************************************************************************\
+ *                          Hinterlib/Neopolitan 2.                          *
+ *                                                                           *
+ *                      Copyright (C) 2019-2023 Aquefir                      *
+ *               Released under Artisan Software Licence v1.1.               *
+\*****************************************************************************/
+
+#ifndef INC_API__HN_BUFFER_H
+#define INC_API__HN_BUFFER_H
+
+/* START sanity check */
+#if !defined( _SYNDEF_FOO ) || !defined( _CFGOPT_FOO )
+#error Your compiler is not configured correctly for using \
+Hinterlib/Neopolitan. Please ensure you are using Slick/Inbound and a \
+compatible compiler (either GCC, Clang, or FCC).
+#endif /* END sanity check */
+
+
+
+#endif /* INC_API__HN_BUFFER_H */
