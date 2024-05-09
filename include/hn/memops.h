8cab02c0e588e363b95b1787a7bb75cc61d37dc8
Alexander Nicholi <me@nicholatian.com>
2024-05-09 03:53:50 -0400

    bifurcate modules for heap handling

--- include/hn/memops.h
+++ include/hn/memops.h
@@ -5,8 +5,8 @@
  *           Released under Artisan Software Licence v1.1.           *
 \*********************************************************************/
 
-#ifndef INC_API__HN_MEMORY_H
-#define INC_API__HN_MEMORY_H
+#ifndef INC_API__HN_MEMOPS_H
+#define INC_API__HN_MEMOPS_H
 
 /* START sanity check */
 #if !defined( _SYNDEF_FOO ) || !defined( _CFGOPT_FOO )
@@ -17,22 +17,15 @@ compatible compiler (either GCC, Clang, or FCC).
 
 #include "types/base.h"
 #include "types/int.h"
-#include "types/mem.h"
-
-/* Attempt to allocate a 16-module. */
-hn_faddr hn_new16mod( void );
-
-/* Attempt to allocate a contiguous array of 16-modules. */
-hn_faddr hn_new16mods( hn_ptri );
 
 /* Set the contents of a stretch of memory, octet-by-octet, to a given
  * value. */
-void hn_memset( void *, hn_ptri, hn_u8 );
+void hn_memset( hn_u8, hn_ptri, void * );
 
 /* Copy some memory from one place to another. */
-void hn_memcpy( void *, void *, hn_ptri );
+void hn_memcpy( void *, hn_ptri, void * );
 
 /* Compare two blocks of memory to see if their contents match. */
-hn_bl hn_memequ( void *, void *, hn_ptri );
+hn_bl hn_memequ( void *, hn_ptri, void * );
 
-#endif /* INC_API__HN_MEMORY_H */
+#endif /* INC_API__HN_MEMOPS_H */
