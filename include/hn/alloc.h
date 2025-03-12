200110c663545a9a758c0ee288c47df0810a3973
Alexander Nicholi <me@nicholatian.com>
2025-03-12 02:09:17 -0400

    boilerplate is 72 columns, not 71

--- include/hn/alloc.h
+++ include/hn/alloc.h
@@ -1,10 +1,10 @@
-/*********************************************************************\
- *                            Hinterlib 2                            *
- *                                                                   *
- *             Copyright (C) 2019-2024 Alexander Nicholi             *
- *          Copyright (C) 2024-2025 Aquefir Consulting LLC.          *
- *           Released under Artisan Software Licence v1.1.           *
-\*********************************************************************/
+/**********************************************************************\
+ *                            Hinterlib v2                            *
+ *                                                                    *
+ *             Copyright (C) 2019-2024 Alexander Nicholi.             *
+ *           Copyright (C) 2024-2025 Aquefir Consulting LLC           *
+ *            Released under Artisan Software Licence v1.1            *
+\**********************************************************************/
 
 #ifndef INC_API__HN_ALLOC_H
 #define INC_API__HN_ALLOC_H
--- util/covreport.py
+++ util/covreport.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-#######################################################################
-##                            Hinterlib 2                            ##
-##                                                                   ##
-##             Copyright (C) 2019-2024 Alexander Nicholi             ##
-##          Copyright (C) 2024-2025 Aquefir Consulting LLC.          ##
-##           Released under Artisan Software Licence v1.1.           ##
-#######################################################################
+########################################################################
+##                            Hinterlib v2                            ##
+##                                                                    ##
+##             Copyright (C) 2019-2024 Alexander Nicholi.             ##
+##           Copyright (C) 2024-2025 Aquefir Consulting LLC           ##
+##            Released under Artisan Software Licence v1.1            ##
+########################################################################
 
 def run(command):
 	import subprocess
