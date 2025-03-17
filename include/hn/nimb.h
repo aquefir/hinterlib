72dc1754bd613f4bf8e5e0ae6076ef612aa8449d
Alexander Nicholi <me@nicholatian.com>
2025-03-16 20:25:16 -0400

    refer to nimbs byref

--- include/hn/nimb.h
+++ include/hn/nimb.h
@@ -54,81 +54,81 @@ typedef struct hn_nimb hn_nimb;
 
 /* Initialise the sentinel bits of a new nimb in memory of a given
  * size. */
-hn_bl hn_nimb_init( hn_ptri, hn_nimb );
+hn_bl hn_nimb_init( hn_ptri, hn_bl, hn_nimb * );
 
 /* Propagate the sentinel bits through the nimb's underlying memory to
  * occupy a new physical storage size. */
-hn_bl hn_nimb_resize( hn_ptri, hn_nimb );
+hn_bl hn_nimb_resize( hn_ptri, hn_nimb * );
 
 /* Create a nimb from a 16-bit unsigned integer primitive. */
-hn_bl hn_nimb_from16( hn_u16, hn_nimb );
+hn_bl hn_nimb_from16( hn_u16, hn_nimb * );
 
 #if defined( _SYNDEF_HAVE_I32 )
 /* Create a nimb from a 32-bit unsigned integer primitive. */
-hn_bl hn_nimb_from32( hn_u32, hn_nimb );
+hn_bl hn_nimb_from32( hn_u32, hn_nimb * );
 #endif /* defined( _SYNDEF_HAVE_I32 ) */
 
 #if defined( _SYNDEF_HAVE_I64 )
 /* Create a nimb from a 64-bit unsigned integer primitive. */
-hn_bl hn_nimb_from64( hn_u64, hn_nimb );
+hn_bl hn_nimb_from64( hn_u64, hn_nimb * );
 #endif /* defined( _SYNDEF_HAVE_I64 ) */
 
 /* Create a 16-bit unsigned integer primitive from a nimb. */
-hn_bl hn_nimb_to16( hn_nimb, hn_u16 * );
+hn_bl hn_nimb_to16( hn_nimb *, hn_u16 * );
 
 #if defined( _SYNDEF_HAVE_I32 )
 /* Create a 32-bit unsigned integer primitive from a nimb. */
-hn_bl hn_nimb_to32( hn_nimb, hn_u32 * );
+hn_bl hn_nimb_to32( hn_nimb *, hn_u32 * );
 #endif /* defined( _SYNDEF_HAVE_I32 ) */
 
 #if defined( _SYNDEF_HAVE_I64 )
 /* Create a 64-bit unsigned integer primitive from a nimb. */
-hn_bl hn_nimb_to64( hn_nimb, hn_u64 * );
+hn_bl hn_nimb_to64( hn_nimb *, hn_u64 * );
 #endif /* defined( _SYNDEF_HAVE_I64 ) */
 
 /* Get the size of the nimb by traversing the sentinel bits therein. */
-hn_ptri hn_nimb_size( hn_nimb );
+hn_ptri hn_nimb_size( hn_nimb * );
 
 /* Perform a full comparison of two nimbs. */
-hn_u8 hn_nimb_cmp( hn_nimb, hn_nimb );
+hn_u8 hn_nimb_cmp( hn_nimb, hn_nimb * );
 
 /* Add two nimbs together. */
-hn_bl hn_nimb_add( hn_nimb, hn_nimb );
+hn_bl hn_nimb_add( hn_nimb, hn_nimb * );
 
 /* Subtract one nimb from another. */
-hn_bl hn_nimb_sub( hn_nimb, hn_nimb );
+hn_bl hn_nimb_sub( hn_nimb, hn_nimb * );
 
 /* Logically shift a nimb left. */
-void hn_nimb_lsl( hn_nimb, hn_nimb );
+void hn_nimb_lsl( hn_nimb, hn_nimb * );
 
 /* Logically shift a nimb right. */
-void hn_nimb_lsr( hn_nimb, hn_nimb );
+void hn_nimb_lsr( hn_nimb, hn_nimb * );
 
 /* Rotate a nimb left. */
-void hn_nimb_rol( hn_nimb, hn_nimb );
+void hn_nimb_rol( hn_nimb, hn_nimb * );
 
 /* Rotate a nimb right. */
-void hn_nimb_ror( hn_nimb, hn_nimb );
+void hn_nimb_ror( hn_nimb, hn_nimb * );
 
 /* Logically AND two nimbs. */
-void hn_nimb_and( hn_nimb, hn_nimb );
+void hn_nimb_and( hn_nimb, hn_nimb * );
 
 /* Logically OR two nimbs. */
-hn_bl hn_nimb_orr( hn_nimb, hn_nimb );
+hn_bl hn_nimb_orr( hn_nimb, hn_nimb * );
 
 /* Logically bit inverse a nimb. */
-void hn_nimb_not( hn_nimb );
+void hn_nimb_not( hn_nimb * );
 
 /* Logically exclusive-OR two nimbs. */
-hn_bl hn_nimb_xor( hn_nimb, hn_nimb );
+hn_bl hn_nimb_xor( hn_nimb, hn_nimb * );
 
 /* Count the leading zero bits of a nimb. */
-hn_bl hn_nimb_clz( hn_nimb, hn_nimb );
+hn_bl hn_nimb_clz( hn_nimb, hn_nimb * );
 
 /* Count the trailing zero bits of a nimb. */
-hn_bl hn_nimb_ctz( hn_nimb, hn_nimb );
+hn_bl hn_nimb_ctz( hn_nimb, hn_nimb * );
 
 /* Count the number of high bits in a nimb. */
-hn_bl hn_nimb_pct( hn_nimb, hn_nimb );
+hn_bl hn_nimb_pct( hn_nimb, hn_nimb * );
 
 #endif /* INC_API__HN_NIMB_H */
