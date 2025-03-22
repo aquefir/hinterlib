c2bc9bf4e5ff0f2b78827aff73b37b4219f03e99
Alexander Nicholi <me@nicholatian.com>
2025-03-21 20:32:20 -0400

    check all possible incoming parameters

--- src/ct.c
+++ src/ct.c
@@ -36,6 +36,8 @@ hn_bl hn_ct_init( hn_u16 sz[4],
 	struct hn_ct * ct )
 {
 	HN_CHK_RETV( type < HN_MAX_CT_TYPE, HN_TRUE );
+	HN_CHK_RETV( ct != NULL, HN_TRUE );
+	HN_CHK_RETV( ct_sz > 0, HN_TRUE );
 
 	if( type != HN_CT_TYPE_FILE )
 	{
