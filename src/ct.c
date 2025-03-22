79bbee6f73b4e17523c1d9fb77a3aabe67a4a81f
Alexander Nicholi <me@nicholatian.com>
2025-03-22 14:28:21 -0400

    use subroutine array and handle file stream backed containers

--- src/ct.c
+++ src/ct.c
@@ -10,6 +10,28 @@
 
 #include <hn/checked.h>
 #include <hn/err.h>
+#include <hn/memops.h>
+
+typedef void (*init_subroutine)( struct hn_ct * );
+
+const init_subroutine init_sub[HN_MAX_CT_TYPE] = { init_flat,
+	init_am8d1,
+	init_am12d1,
+	init_am16d1,
+	init_am20d1,
+	init_am8d2,
+	init_am12d2,
+	init_am16d2,
+	init_am20d2,
+	init_am8d3,
+	init_am12d3,
+	init_am16d3,
+	init_am20d3,
+	init_am8d4,
+	init_am12d4,
+	init_am16d4,
+	init_am20d4,
+	init_file };
 
 const u8 ct_bits[HN_MAX_CT_TYPE] = { 0,
 	8,
@@ -29,6 +51,45 @@ const u8 ct_bits[HN_MAX_CT_TYPE] = { 0,
 	16,
 	20,
 	0 };
+
+void init_flat( struct hn_ct * ct )
+{
+	ct->data.flat = (u8 *)ct + sizeof( *ct );
+}
+
+void init_am8d1( struct hn_ct * ct ) {}
+
+void init_am12d1( struct hn_ct * ct ) {}
+
+void init_am16d1( struct hn_ct * ct ) {}
+
+void init_am20d1( struct hn_ct * ct ) {}
+
+void init_am8d2( struct hn_ct * ct ) {}
+
+void init_am12d2( struct hn_ct * ct ) {}
+
+void init_am16d2( struct hn_ct * ct ) {}
+
+void init_am20d2( struct hn_ct * ct ) {}
+
+void init_am8d3( struct hn_ct * ct ) {}
+
+void init_am12d3( struct hn_ct * ct ) {}
+
+void init_am16d3( struct hn_ct * ct ) {}
+
+void init_am20d3( struct hn_ct * ct ) {}
+
+void init_am8d4( struct hn_ct * ct ) {}
+
+void init_am12d4( struct hn_ct * ct ) {}
+
+void init_am16d4( struct hn_ct * ct ) {}
+
+void init_am20d4( struct hn_ct * ct ) {}
+
+void init_file( struct hn_ct * ct ) {}
 
 hn_bl hn_ct_init( hn_u16 sz[4],
 	enum hn_ct_type type,
@@ -70,6 +131,17 @@ hn_bl hn_ct_init( hn_u16 sz[4],
 
 		HN_CHK_RETV( tmp <= ct_sz, HN_TRUE );
 	}
+	else
+	{
+		const ptri sz = sizeof(struct hn_ct_file) + HN_PATHMAX + 1 + sizeof(struct hn_ct);
+
+		HN_CHK_RETV( sz <= ct_sz, HN_TRUE );
+	}
+
+	hn_memset( 0, ct_sz, ct );
+	ct->type = type;
+
+	init_sub[type]( ct );
 
 	return HN_FALSE;
 }
