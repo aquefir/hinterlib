96f413f785b2a3f704133cedd72cfc377239b2a6
Alexander Nicholi <me@nicholatian.com>
2025-04-03 11:38:50 -0400

    autoformat

--- include/hn/checksum.h
+++ include/hn/checksum.h
@@ -14,40 +14,40 @@
 
 #include "ct.h"
 
-hn_bl hn_crc16( struct hn_ct *, hn_u8 (*)[2] );
+hn_bl hn_crc16( struct hn_ct *, hn_u8 ( * )[2] );
 
-hn_bl hn_crc32( struct hn_ct *, hn_u8 (*)[4] );
+hn_bl hn_crc32( struct hn_ct *, hn_u8 ( * )[4] );
 
-hn_bl hn_crc64( struct hn_ct *, hn_u8 (*)[8] );
+hn_bl hn_crc64( struct hn_ct *, hn_u8 ( * )[8] );
 
-hn_bl hn_md4( struct hn_ct *, hn_u8 (*)[16] );
+hn_bl hn_md4( struct hn_ct *, hn_u8 ( * )[16] );
 
-hn_bl hn_md5( struct hn_ct *, hn_u8 (*)[16] );
+hn_bl hn_md5( struct hn_ct *, hn_u8 ( * )[16] );
 
-hn_bl hn_sha1( struct hn_ct *, hn_u8 (*)[20] );
+hn_bl hn_sha1( struct hn_ct *, hn_u8 ( * )[20] );
 
-hn_bl hn_sha2_224( struct hn_ct *, hn_u8 (*)[28] );
+hn_bl hn_sha2_224( struct hn_ct *, hn_u8 ( * )[28] );
 
-hn_bl hn_sha2_256( struct hn_ct *, hn_u8 (*)[32] );
+hn_bl hn_sha2_256( struct hn_ct *, hn_u8 ( * )[32] );
 
-hn_bl hn_sha2_384( struct hn_ct *, hn_u8 (*)[48] );
+hn_bl hn_sha2_384( struct hn_ct *, hn_u8 ( * )[48] );
 
-hn_bl hn_sha2_512( struct hn_ct *, hn_u8 (*)[64] );
+hn_bl hn_sha2_512( struct hn_ct *, hn_u8 ( * )[64] );
 
-hn_bl hn_sha2_512_224( struct hn_ct *, hn_u8 (*)[28] );
+hn_bl hn_sha2_512_224( struct hn_ct *, hn_u8 ( * )[28] );
 
-hn_bl hn_sha2_512_256( struct hn_ct *, hn_u8 (*)[32] );
+hn_bl hn_sha2_512_256( struct hn_ct *, hn_u8 ( * )[32] );
 
-hn_bl hn_sha3_224( struct hn_ct *, hn_u8 (*)[28] );
+hn_bl hn_sha3_224( struct hn_ct *, hn_u8 ( * )[28] );
 
-hn_bl hn_sha3_256( struct hn_ct *, hn_u8 (*)[32] );
+hn_bl hn_sha3_256( struct hn_ct *, hn_u8 ( * )[32] );
 
-hn_bl hn_sha3_384( struct hn_ct *, hn_u8 (*)[48] );
+hn_bl hn_sha3_384( struct hn_ct *, hn_u8 ( * )[48] );
 
-hn_bl hn_sha3_512( struct hn_ct *, hn_u8 (*)[64] );
+hn_bl hn_sha3_512( struct hn_ct *, hn_u8 ( * )[64] );
 
-hn_bl hn_shake128( struct hn_ct *, hn_u8 (*)[16] );
+hn_bl hn_shake128( struct hn_ct *, hn_u8 ( * )[16] );
 
-hn_bl hn_shake256( struct hn_ct *, hn_u8 (*)[32] );
+hn_bl hn_shake256( struct hn_ct *, hn_u8 ( * )[32] );
 
 #endif /* INC_API__HN_CHECKSUM_H */
