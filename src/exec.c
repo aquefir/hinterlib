f6f5332e6060ddf41dfe57939310a0a3c1fcda2b
Alexander Nicholi <me@nicholatian.com>
2025-04-08 22:01:09 -0400

    implement hn_getcwd()

--- src/exec.c
+++ src/exec.c
@@ -5,3 +5,14 @@
  *           Copyright (C) 2024-2025 Aquefir Consulting LLC           *
  *            Released under Artisan Software Licence v1.1            *
 \**********************************************************************/
+
+#include <hn/exec.h>
+
+#include <unistd.h>
+
+bl hn_getcwd( chr p[HN_PATHMAX + 1] )
+{
+	chr * const ret = getcwd( p, HN_PATHMAX );
+
+	return ret == NULL ? HN_TRUE : HN_FALSE;
+}
