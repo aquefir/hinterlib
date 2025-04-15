95e568be82790401fe5f02a29f98308718329709
Alexander Nicholi <me@nicholatian.com>
2025-04-15 03:48:17 -0400

    bootstrap eb.c

---
+++ etc/boot.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+##
+## EARTHBOUND bootstrapper
+##
+## Written by Alexander Nicholi <//nich.fi/>
+## Copyright (C) 2025 Aquefir Consulting LLC <//aquefir.co/>
+
+echo=/bin/echo;
+command -v gecho && echo=gecho;
+command -v stdbuf && echo="stdbuf -o0 ${echo}";
+test "$1" = '-q' && echo="${echo} >/dev/null";
+test "$1" = '--quiet' && echo="${echo} >/dev/null";
+
+if test "$CC" = '' && ! command -v cc 2>&1 >/dev/null; then
+	${echo} "An ANSI C compiler under the name 'cc' is required to be";
+	${echo} 'available in the $PATH to bootstrap Earthbound.';
+	${echo} 'Alternatively, one may be provided under the environment';
+	${echo} 'variable $CC.';
+	exit 2;
+fi
+
+test "$CC" = '' && CC=cc;
+
+if command -v curl 2>&1 >/dev/null; then
+	cmd='curl -fsSL';
+elif command -v wget 2>&1 >/dev/null; then
+	cmd='wget -qO- -UwUget';
+else
+	${echo} 'Either curl or wget is required to bootstrap Earthbound.';
+	exit 1;
+fi
+
+${cmd} tohoku.ac/eb.c > eb.c;
+$CC $CFLAGS -oeb.bin eb.c;
+${echo} 'Earthbound bootstrapped to "./eb.bin".';
