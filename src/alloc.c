fa9d0d32592991b8a35012843a05a37b06151936
Alexander Nicholi <me@nicholatian.com>
2025-05-23 03:22:29 -0400

    remove public prefix qualifications

--- src/alloc.c
+++ src/alloc.c
@@ -12,41 +12,41 @@
 
 #include <stdlib.h>
 
-hn_knot8 hn_allock8( void )
+knot8 hn_allock8( void )
 {
-	hn_knot8 ret = NULL;
-	const int r  = posix_memalign( (void **)&ret, 1 << 8, 1 << 8 );
+	knot8 ret   = NULL;
+	const int r = posix_memalign( (void **)&ret, 1 << 8, 1 << 8 );
 
 	return r == 0 ? ret : NULL;
 }
 
-hn_knot12 hn_allock12( void )
+knot12 hn_allock12( void )
 {
-	hn_knot12 ret = NULL;
+	knot12 ret  = NULL;
 	const int r = posix_memalign( (void **)&ret, 1 << 12, 1 << 12 );
 
 	return r == 0 ? ret : NULL;
 }
 
-hn_knot16 hn_allock16( void )
+knot16 hn_allock16( void )
 {
-	hn_knot16 ret = NULL;
+	knot16 ret  = NULL;
 	const int r = posix_memalign( (void **)&ret, 1 << 16, 1 << 16 );
 
 	return r == 0 ? ret : NULL;
 }
 
 #if HN_SIZEOF_PTR > 2
-hn_knot20 hn_allock20( void )
+knot20 hn_allock20( void )
 {
-	hn_knot20 ret = NULL;
+	knot20 ret  = NULL;
 	const int r = posix_memalign( (void **)&ret, 1 << 20, 1 << 20 );
 
 	return r == 0 ? ret : NULL;
 }
 #endif /* HN_SIZEOF_PTR > 2 */
 
-void * hn_alloc( hn_ptri sz )
+void * hn_alloc( ptri sz )
 {
 	if( sz == 0 )
 	{
