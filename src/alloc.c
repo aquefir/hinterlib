ec869e689621f200d12ce5b5fdb9a10af4ec981e
Alexander Nicholi <me@nicholatian.com>
2025-05-23 03:27:14 -0400

    implement multi-knot allocators, completing #96

--- src/alloc.c
+++ src/alloc.c
@@ -12,12 +12,37 @@
 
 #include <stdlib.h>
 
+#include <hn/err.h>
+
 knot8 hn_allock8( void )
 {
 	knot8 ret   = NULL;
 	const int r = posix_memalign( (void **)&ret, 1 << 8, 1 << 8 );
 
 	return r == 0 ? ret : NULL;
+}
+
+ptri hn_allock8a( ptri n, knot8 * ret )
+{
+	ptri i;
+
+	HN_CHK_RETV( ret != NULL, 0 );
+
+	for( i = 0; i < n; ++i )
+	{
+		const knot8 k = hn_allock8( );
+
+		if( k != NULL )
+		{
+			ret[i] = k;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return i;
 }
 
 knot12 hn_allock12( void )
@@ -28,12 +53,58 @@ knot12 hn_allock12( void )
 	return r == 0 ? ret : NULL;
 }
 
+ptri hn_allock12a( ptri n, knot12 * ret )
+{
+	ptri i;
+
+	HN_CHK_RETV( ret != NULL, 0 );
+
+	for( i = 0; i < n; ++i )
+	{
+		const knot12 k = hn_allock12( );
+
+		if( k != NULL )
+		{
+			ret[i] = k;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return i;
+}
+
 knot16 hn_allock16( void )
 {
 	knot16 ret  = NULL;
 	const int r = posix_memalign( (void **)&ret, 1 << 16, 1 << 16 );
 
 	return r == 0 ? ret : NULL;
+}
+
+ptri hn_allock16a( ptri n, knot16 * ret )
+{
+	ptri i;
+
+	HN_CHK_RETV( ret != NULL, 0 );
+
+	for( i = 0; i < n; ++i )
+	{
+		const knot16 k = hn_allock16( );
+
+		if( k != NULL )
+		{
+			ret[i] = k;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return i;
 }
 
 #if HN_SIZEOF_PTR > 2
@@ -43,6 +114,29 @@ knot20 hn_allock20( void )
 	const int r = posix_memalign( (void **)&ret, 1 << 20, 1 << 20 );
 
 	return r == 0 ? ret : NULL;
+}
+
+ptri hn_allock20a( ptri n, knot20 * ret )
+{
+	ptri i;
+
+	HN_CHK_RETV( ret != NULL, 0 );
+
+	for( i = 0; i < n; ++i )
+	{
+		const knot20 k = hn_allock20( );
+
+		if( k != NULL )
+		{
+			ret[i] = k;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return i;
 }
 #endif /* HN_SIZEOF_PTR > 2 */
 
