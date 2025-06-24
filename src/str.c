07d2cc47d568927e91fd6ca903de7fac8eb5ebb7
Alexander Nicholi <me@nicholatian.com>
2025-06-24 03:55:29 -0400

    unfinished

---
+++ src/str.c
@@ -0,0 +1,9 @@
+/**********************************************************************\
+ *                            Hinterlib v2                            *
+ *                                                                    *
+ *             Copyright (C) 2019-2024 Alexander Nicholi.             *
+ *           Copyright (C) 2024-2025 Aquefir Consulting LLC           *
+ *            Released under Artisan Software Licence v1.1            *
+\**********************************************************************/
+
+#include <hn/str.h>
